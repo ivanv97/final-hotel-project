cf214837bf80a15bdae05c3ab0b1ca46a5e68d6d
Adelina Mihaylova <aggmihaylova@gmail.com>
2019-08-06 10:16:13 +0300

    ready to merge

--- src/main/java/eu/deltasource/internship/hotel/domain/Booking.java
+++ src/main/java/eu/deltasource/internship/hotel/domain/Booking.java
@@ -1,10 +1,5 @@
 package eu.deltasource.internship.hotel.domain;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import eu.deltasource.internship.hotel.exception.FailedInitializationException;
 import lombok.Data;
 
@@ -16,57 +11,57 @@ import java.time.LocalDate;
 @Data
 public class Booking {
 
-    private final int bookingId;
-    private final int guestId;
-    private final int roomId;
-    private int numberOfPeople;
-    private LocalDate from;
-    private LocalDate to;
+	private final int bookingId;
+	private final int guestId;
+	private final int roomId;
+	private int numberOfPeople;
+	private LocalDate from;
+	private LocalDate to;
 
-    public Booking(int bookingId, int guestId, int roomId, int numberOfPeople, LocalDate from, LocalDate to) {
-        this.bookingId = bookingId;
-        this.guestId = guestId;
-        this.roomId = roomId;
-        this.numberOfPeople = numberOfPeople;
-        setBookingDates(from, to);
-    }
+	public Booking(int bookingId, int guestId, int roomId, int numberOfPeople, LocalDate from, LocalDate to) {
+		this.bookingId = bookingId;
+		this.guestId = guestId;
+		this.roomId = roomId;
+		this.numberOfPeople = numberOfPeople;
+		setBookingDates(from, to);
+	}
 
-    /**
-     * This constructor should be used
-     * only by the repository.
-     */
-    public Booking(Booking booking) {
-        this.bookingId = booking.bookingId;
-        this.guestId = booking.guestId;
-        this.roomId = booking.roomId;
-        this.numberOfPeople = booking.numberOfPeople;
-        setBookingDates(booking.from, booking.to);
-    }
+	/**
+	 * This constructor should be used
+	 * only by the repository.
+	 */
+	public Booking(Booking booking) {
+		this.bookingId = booking.bookingId;
+		this.guestId = booking.guestId;
+		this.roomId = booking.roomId;
+		this.numberOfPeople = booking.numberOfPeople;
+		setBookingDates(booking.from, booking.to);
+	}
 
-    public void setBookingDates(LocalDate from, LocalDate to) {
-        try {
-            if (from.isAfter(to) || to.equals(from) || from.isBefore(LocalDate.now())) {
-                throw new FailedInitializationException("Invalid dates given!");
-            }
-            this.from = from;
-            this.to = to;
-        } catch (NullPointerException npe) {
-            throw new FailedInitializationException("Date parameters are null!");
-        }
-    }
+	public void setBookingDates(LocalDate from, LocalDate to) {
+		try {
+			if (from.isAfter(to) || to.equals(from) || from.isBefore(LocalDate.now())) {
+				throw new FailedInitializationException("Invalid dates given!");
+			}
+			this.from = from;
+			this.to = to;
+		} catch (NullPointerException npe) {
+			throw new FailedInitializationException("Date parameters are null!");
+		}
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        } else if (!(obj instanceof Booking)) {
-            return false;
-        }
-        return bookingId == ((Booking) obj).bookingId;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		} else if (!(obj instanceof Booking)) {
+			return false;
+		}
+		return bookingId == ((Booking) obj).bookingId;
+	}
 
-    @Override
-    public int hashCode() {
-        return bookingId;
-    }
+	@Override
+	public int hashCode() {
+		return bookingId;
+	}
 }
--- src/main/java/eu/deltasource/internship/hotel/repository/GuestRepository.java
+++ src/main/java/eu/deltasource/internship/hotel/repository/GuestRepository.java
@@ -15,7 +15,6 @@ import java.util.List;
 @Repository
 public class GuestRepository {
 
-<<<<<<< HEAD
 	private final List<Guest> repository;
 
 	/**
@@ -83,6 +82,7 @@ public class GuestRepository {
 	/**
 	 * Saves all given items in the repository
 	 */
+
 	public void saveAll(Guest... items) {
 		saveAll(Arrays.asList(items));
 	}
@@ -95,7 +95,6 @@ public class GuestRepository {
 				return new Guest(guest);
 			}
 		}
-
 		throw new ItemNotFoundException("Guest not found in repository!");
 	}
 
@@ -133,122 +132,3 @@ public class GuestRepository {
 		return repository.size();
 	}
 }
-=======
-    private final List<Guest> repository;
-
-    /**
-     * Default constructor, which initializes the repository
-     * as an empty ArrayList.
-     */
-    public GuestRepository() {
-        repository = new ArrayList<>();
-    }
-
-    /**
-     * Returns an unmodifiable list of all items
-     * currently in the repository.
-     */
-    public List<Guest> findAll() {
-        return Collections.unmodifiableList(repository);
-    }
-
-    /**
-     * Method, which checks the repository if
-     * there is an item available with the given id.
-     * <p>
-     * Check this always, before using operations with id's.
-     */
-    public boolean existsById(int id) {
-        for (Guest item : repository) {
-            if (item.getGuestId() == id)
-                return true;
-        }
-        return false;
-    }
-
-    /**
-     * Returns a copy of the item from the repository
-     * with the given Id.
-     */
-    public Guest findById(int id) {
-        for (Guest item : repository) {
-            if (item.getGuestId() == id)
-                return new Guest(item);
-        }
-        throw new ItemNotFoundException("A Guest with id: " + id + " was not found!");
-    }
-
-    private int idGenerator() {
-        if (count() == 0) {
-            return count() + 1;
-        }
-        return repository.get(count() - 1).getGuestId() + 1;
-    }
-
-    public void save(Guest item) {
-        Guest newGuest = new Guest(idGenerator(), item.getFirstName(), item.getLastName(), item.getGender());
-        repository.add(newGuest);
-    }
-
-    /**
-     * Saves the list of items in the repository
-     */
-    public void saveAll(List<Guest> items) {
-        items.forEach(
-                this::save);
-    }
-
-    /**
-     * Saves all given items in the repository
-     */
-
-    public void saveAll(Guest... items) {
-        saveAll(Arrays.asList(items));
-    }
-
-    public Guest updateGuest(Guest item) {
-        for (Guest guest : repository) {
-            if (guest.getGuestId() == item.getGuestId()) {
-                guest.changeGender(item.getGender());
-                guest.changeFirstAndLastNames(item.getFirstName(), item.getLastName());
-                return new Guest(guest);
-            }
-        }
-        throw new ItemNotFoundException("Guest not found in repository!");
-    }
-
-    /**
-     * Removes an item from the repository
-     * by searching for an exact match.
-     * <p>
-     * Returns true if an exact match is and deleted,
-     * returns false if there's no match and the list is unchanged.
-     */
-    public boolean delete(Guest item) {
-        return repository.remove(item);
-    }
-
-    public boolean deleteById(int id) {
-        for (Guest guest : repository) {
-            if (guest.getGuestId() == id) {
-                return delete(guest);
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Deletes all items in the repository
-     */
-    public void deleteAll() {
-        repository.clear();
-    }
-
-    /**
-     * Returns the number of items left in the repository
-     */
-    public int count() {
-        return repository.size();
-    }
-}
->>>>>>> improvments after review
